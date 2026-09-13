37bd5fd574a6a66b927876cb1e4dce338e94e445
pritam <pritamahrod22@gmail.com>
2026-09-13 14:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.30%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int n) {
+        
+         if(n==0) return 0;
+        int lo = 1, hi = n;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(mid > n/mid) hi = mid - 1;
+            else if(mid < n/mid) lo = mid + 1;
+            else return mid;
+        }
+        return hi;
+    }
+    
+};
